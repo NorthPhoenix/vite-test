1219542fb9c603d7ed57f0cb2707c97cf1ed2e32
Nikita Istomin <nikisr54@gmail.com>
2024-01-14 16:11:27 -0600

    test

---
+++ api/hello.js
@@ -0,0 +1,7 @@
+export default function handler(request, response) {
+  response.status(200).json({
+    body: `Hello from lambda! ${(Math.random() * 1000).toFixed(0)}`,
+    query: request.query,
+    cookies: request.cookies,
+  });
+}
